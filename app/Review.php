474ce6fdb1236fb3b0b3b64aa9c839412257231b
hapo-nghialt <nghia.lt@haposoft.com>
2020-08-21 14:33:22 +0700

    edit model review

--- app/Review.php
+++ app/Review.php
@@ -20,11 +20,11 @@ class Review extends Model
 
     public function course()
     {
-        return $this->belongsTo(Course::class, 'target_id')->where('target_id', 0);
+        return $this->belongsTo(Course::class, 'target_id')->where('type', 0);
     }
 
     public function lesson()
     {
-        return $this->belongsTo(Lesson::class, 'target_id')->where('target_id', 1);
+        return $this->belongsTo(Lesson::class, 'target_id')->where('type', 1);
     }
 }
