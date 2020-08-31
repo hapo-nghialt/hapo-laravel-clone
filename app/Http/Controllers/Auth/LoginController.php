4abfba745e51fc64389b97cf111bf934c40063e5
hapo-nghialt <nghia.lt@haposoft.com>
2020-08-31 13:47:33 +0700

    edit phpcs

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -59,4 +59,19 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson() ? new Response('', 204) : redirect('/');
+    }
 }
