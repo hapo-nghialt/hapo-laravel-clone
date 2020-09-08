dfb6d2449182ee99b4a4a480e2677989b19e176e
hapo-nghialt <nghia.lt@haposoft.com>
2020-09-08 14:54:48 +0700

    edit font-awesome

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .copy('node_modules/font-awesome/fonts/', 'public/fonts')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
     .options({
         processCssUrls: false
     });
