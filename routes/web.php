ef6bc4728aee1c59c1fe6c363783c157eadbae85
hapo-nghialt <nghia.lt@haposoft.com>
2020-09-14 11:53:09 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the 'web' middleware group. Now create something great!
 |
 */
 
@@ -21,7 +21,7 @@ Route::get('/course-detail/{id}', 'CourseController@showCourseDetail')->name('co
 Route::get('/lesson-detail/{id}', 'LessonController@show')->name('lesson.detail');
 Route::get('/search-course-detail/{id}', 'CourseController@searchCourseDetail')->name('course.detail.search');
 Route::post('take-course/{id}', 'CourseController@takeCourse')->name('take.course');
-Route::post('leave-course/{id}', "CourseController@leaveCourse")->name('leave.course');
+Route::post('leave-course/{id}', 'CourseController@leaveCourse')->name('leave.course');
 Route::post('take-lesson/{id}', 'LessonController@takeLesson')->name('take.lesson');
 Route::post('course/review/store/{id}', 'ReviewController@storeCourseReview')->name('review.course.store');
 Route::delete('course/review/delete/{id}', 'ReviewController@destroyReview')->name('review.destroy');
